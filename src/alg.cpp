31d685ea7fa393a608ad7c17bcd07c7f0e5e044a
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-03-13 15:20:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,21 +10,30 @@ bool checkPrime(uint64_t value) {
   }
   return true;
 }
-uint64_t nPrime(uint64_t k) {
-  uint64_t b = 0;
+uint64_t nPrime(uint64_t n) {
+  uint64_t s = 0;
   uint64_t m = 0;
-  while (m != k) {
-    b++;
-    if (checkPrime(b))
+  while (m != n) {
+    s++;
+    if (checkPrime(s))
       m++;
   }
-  return b;
+  return s;
 }
 
 uint64_t nextPrime(uint64_t value) {
   value++;
-   
+    while (checkPrime (value) != true) {
     while (checkPrime(value) != true) {
       value++;
     }
     return value;
+}
+uint64_t sumPrime(uint64_t hbound) {
+  uint64_t sum = 0;
+    for (uint64_t m = 2; m < hbound; m++) {
+      if (checkPrime(k) == true)
+        sum += m;
+    }
+    return sum;
+}
